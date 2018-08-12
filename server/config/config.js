a8422afc5903ebc6030f95be16e3d900e1d8a7c1
sancardenasv <sancardenasv@gmail.com>
2018-08-12 14:30:04 -0500

    environment variable of mongo DB

--- server/config/config.js
+++ server/config/config.js
@@ -11,9 +11,9 @@ process.env.PROFILE = process.env.PROFILE || 'dev';
  */
 let mongoDBUrl;
 if (process.env.PROFILE === 'dev') {
-    mongoDBUrl = 'mongodb://localhost:27017/cafe'
+    mongoDBUrl = 'mongodb://localhost:27017/cafe';
 } else {
-    mongoDBUrl = 'mongodb://cafe-user:cafe-user123456*@ds119652.mlab.com:19652/cafe'
+    mongoDBUrl = process.env.MONGO_URI;
 }
 
 process.env.MONGO_DB_URL = mongoDBUrl;
